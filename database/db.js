f14d9279806c220491916a3c9c1d535266e04259
Elias <eliassilva.c@hotmail.com>
2026-04-15 21:50:42 -0300

    Alterando configuração de credencial do bancoo de dados

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: '10.68.102.50',
-    database: 'financeiro',
-    password: '123456',
+    user: 'postgres.fmujfcjrtucztxuvdlma',
+    host: 'aws-1-us-east-1.pooler.supabase.com',
+    database: 'postgres',
+    password: 'Elias13122ooo!14',
     port: 5432
 })
 
